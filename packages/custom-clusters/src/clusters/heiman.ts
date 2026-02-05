e31d2244e813ba961a698d1e05c2e1ab69f299af
Leo2442926161 <2442926161@qq.com>
2026-02-05 16:41:57 +0800

    fix heiman attributes

--- packages/custom-clusters/src/clusters/heiman.ts
+++ packages/custom-clusters/src/clusters/heiman.ts
@@ -8,24 +8,24 @@ import { attribute, cluster, uint8 } from "@matter/main/model";
 
 @cluster(0x120bfc01)
 export class HeimanCluster {
-    @attribute(0x00120b0010, uint8)
+    @attribute(0x0010, uint8)
     tamperAlarm?: number;
 
-    @attribute(0x00120b0011, uint8)
+    @attribute(0x0011, uint8)
     preheatingState?: number;
 
-    @attribute(0x00120b0012, uint8)
+    @attribute(0x0012, uint8)
     noDisturbingState?: number;
 
-    @attribute(0x00120b0013, uint8)
+    @attribute(0x0013, uint8)
     sensorType?: number;
 
-    @attribute(0x00120b0014, uint8)
+    @attribute(0x0014, uint8)
     sirenActive?: number;
 
-    @attribute(0x00120b0015, uint8)
+    @attribute(0x0015, uint8)
     alarmMute?: number;
 
-    @attribute(0x00120b0016, uint8)
+    @attribute(0x0016, uint8)
     lowPowerMode?: number;
 }
