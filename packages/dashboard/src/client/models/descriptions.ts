d14dc9b0e5a4959eb384b5dbae3181fedec528d8
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2026-05-12 21:14:28 +0200

    Add `WindowOpenMode` description to models

--- packages/dashboard/src/client/models/descriptions.ts
+++ packages/dashboard/src/client/models/descriptions.ts
@@ -15204,6 +15204,13 @@ export const clusters: Record<number, ClusterDescription> = {
                 "type": "Optional[unknown]",
                 "writable": true
             },
+            "319422487": {
+                "id": 319422487,
+                "cluster_id": 319486977,
+                "label": "WindowOpenMode",
+                "type": "Optional[bool]",
+                "writable": false
+            },
             "319422488": {
                 "id": 319422488,
                 "cluster_id": 319486977,
